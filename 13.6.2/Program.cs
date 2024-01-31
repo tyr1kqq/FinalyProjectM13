30a8e068e47d523692fb87428e0d8e5d5db20151
tyr1kqq <igorturkinwhite@yandex.ru>
2024-01-31 17:04:52 +0200

    Task2End

---
+++ 13.6.2/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+
+namespace _13._6._2
+{
+    class Program
+    {
+        static void Main()
+        {
+            string FilePath = @"C:\\Users\tyr1k_qq\Desktop\List\test.txt";
+            string text = File.ReadAllText(FilePath);
+
+            string[] sings = text.Split(new char[] { ' ', '.', ',', '!', '?' }, StringSplitOptions.RemoveEmptyEntries);
+
+            
+            Dictionary<string, int> wordCount = new Dictionary<string, int>();
+            foreach (string word in sings)
+            {
+                if (wordCount.ContainsKey(word))
+                {
+                    wordCount[word]++;
+                }
+                else
+                {
+                    wordCount[word] = 1;
+                }
+            }
+
+            var topWords = wordCount.OrderByDescending(pair => pair.Value).Take(10);
+
+            
+            Console.WriteLine("Наиболее часто встречающиеся слова:");
+            foreach (var wordPair in topWords)
+            {
+                Console.WriteLine($"{wordPair.Key} - {wordPair.Value} раз");
+            }
+        }
+    }
+}
