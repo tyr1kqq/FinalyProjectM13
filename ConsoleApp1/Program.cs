5e4f18aa15806b57475527cf50b127c0cc9f8200
tyr1kqq <igorturkinwhite@yandex.ru>
2024-01-31 16:42:36 +0200

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+class Program
+{
+    static void Main()
+    {
+        string path = "С:\\Users\\tyr1k_qq\\Desktop\\test.txt";
+
+        List<string> LissTTest = new List<string>();
+
+        // Считываем содержимое файла и сохраняем его в список
+        try
+        {
+            LissTTest = File.ReadAllLines(path).ToList();
+        }
+        catch (IOException ex)
+        {
+            Console.WriteLine("Произошла ошибка при чтении файла: " + ex.Message);
+            return;
+        }
+
+        // Выводим содержимое списка
+        foreach (string строка in LissTTest)
+        {
+            Console.WriteLine(строка);
+        }
+    }
+}
---
+++ FinalyProjectM13/Program.cs
@@ -0,0 +1,44 @@
+﻿using System.IO;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace FinalyProjectM13
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var Timer = Stopwatch.StartNew();
+
+            StreamReader sr = new StreamReader(@"C:\\Users\tyr1k_qq\Desktop\List\test.txt");
+            ListTimer(sr);
+
+            Console.WriteLine($"Вставка в List T :  {Timer.Elapsed.TotalMilliseconds}  мс");
+
+            Timer.Restart();
+            Timer.Start();
+
+            LinkedListTimer(sr);
+            Console.WriteLine($"Вставка в LinkedList T :  {Timer.Elapsed.TotalMilliseconds}  мс");
+
+        }
+        static void ListTimer(StreamReader sr)
+        {
+
+            List<String> list = new List<String>();
+            while (!sr.EndOfStream)
+            {
+                list.Add(sr.ReadLine());
+            }
+        }
+
+        static void LinkedListTimer(StreamReader sr)
+        {
+            LinkedList<String> LinkList = new LinkedList<String>();
+            while (!sr.EndOfStream)
+            {
+                LinkList.AddLast(sr.ReadLine());
+            }
+        }
+    }
+}
